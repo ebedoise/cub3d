ee9ebc9bd0253c12c33ed0a44a81e22169080f1e
Emile BEDOISEAU <embedois@e1r3p17.clusters.42paris.fr>
2022-10-15 07:53:18 +0200

    colors

--- srcs/checks/cardinal_checks.c
+++ srcs/checks/cardinal_checks.c
@@ -1,7 +1,6 @@
 #include "cub.h"
 
 // add checks for the textures when implemented
-// if multiple NO SO EA WE ??
 int	__cardinals(char *str, t_game *g)
 {
 	char	**strs;
@@ -14,6 +13,11 @@ int	__cardinals(char *str, t_game *g)
 		__free_split(strs);
 		return (1);
 	}
+	if ((__strcmp(strs[0], "NO") == 0 && g->no != NULL)
+		|| (__strcmp(strs[0], "SO") == 0 && g->so != NULL)
+		|| (__strcmp(strs[0], "WE") == 0 && g->we != NULL)
+		|| (__strcmp(strs[0], "EA") == 0 && g->ea != NULL))
+		return (1);
 	if (__strcmp(strs[0], "NO") == 0)
 		g->no = __strdup(strs[1]);
 	else if (__strcmp(strs[0], "SO") == 0)
@@ -28,18 +32,36 @@ int	__cardinals(char *str, t_game *g)
 
 int	__colors(char *str, t_game *g, char c)
 {
-	(void)str;
+	int	i;
+
+	i = 2;
 	if (c == 'f')
 	{
-		g->f.r = 1;
-		g->f.g = 1;
-		g->f.b = 1;
+		g->f.r = __atoi(str, &i);
+		if (str[i] != ',' || g->f.r < 0 || g->f.r > 255)
+			return (1);
+		i++;
+		g->f.g = __atoi(str, &i);
+		if (str[i] != ',' || g->f.g < 0 || g->f.g > 255)
+			return (1);
+		i++;
+		g->f.b = __atoi(str, &i);
+		if (str[i] || g->f.b < 0 || g->f.b > 255)
+			return (1);
 	}
 	else if (c == 'c')
 	{
-		g->c.r = 1;
-		g->c.g = 1;
-		g->c.b = 1;
+		g->c.r = __atoi(str, &i);
+		if (str[i] != ',' || g->c.r < 0 || g->c.r > 255)
+			return (1);
+		i++;
+		g->c.g = __atoi(str, &i);
+		if (str[i] != ',' || g->c.g < 0 || g->c.g > 255)
+			return (1);
+		i++;
+		g->c.b = __atoi(str, &i);
+		if (str[i] || g->c.b < 0 || g->c.b > 255)
+			return (1);
 	}
 	return (0);
 }
--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -96,3 +96,28 @@ char	*__strndup(char *s1, int n)
 	free(s1);
 	return (array);
 }
+
+int	__atoi(char *str, int *i)
+{
+	int	res;
+	int	sign;
+
+	sign = 1;
+	res = 0;
+	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == ' ')
+		(*i)++;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			sign *= -1;
+		(*i)++;
+	}
+	while (str[*i] && (str[*i] >= '0' && str[*i] <= '9'))
+	{
+		res = res * 10 + (str[*i] - 48);
+		(*i)++;
+	}
+	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == ' ')
+		(*i)++;
+	return (res * sign);
+}
